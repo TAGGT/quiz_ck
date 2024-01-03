0db58b5abcd4e92f6668bd67ce509033946c2355
TAGGT <take.goto.0120@gmail.com>
2024-01-03 20:42:59 +0900

    add navigation to create and home link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,12 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('create')" :active="request()->routeIs('dashboard')">
+                        {{ __('Create') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('dashboard')">
+                        {{ __('Home') }}
+                    </x-nav-link>
                 </div>
             </div>
 
